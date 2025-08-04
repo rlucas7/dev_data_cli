996f9620ae28f443930843c5b8e7d2031acd8287
Lucas Roberts <rlucas7@Lucass-MacBook-Air.local>
2025-08-04 15:57:44 -0400

    add basic plots flag
      * this commit adds the `-p` flag to the cli tool
       using this flag causes a separate plotly server
       to be spawned that plots the number of characters
       against the time of the completion.

--- cli.js
+++ cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 // commander or yargs are options
+const { plot } = require('nodeplotlib');
 const { Command } = require('commander');
 const program = new Command();
 const fs = require('fs');
 const readline = require('readline');
 const verbose = false;
+
 
 //TODO: setup logging for this script
 var processJsonl = async function processJsonlFile(filePath, opts) {
@@ -36,25 +38,52 @@ var processJsonl = async function processJsonlFile(filePath, opts) {
     let autoCompletionEntries = [];
     console.log(autoCompletionEntries[autoCompletionEntries.length-1]);
     // const keepKeys = ["timestamp", "modelName", "eventName", "prompt", "completion", "accepted", "filepath"];
+    let acceptedCounter = 0, rejectedCounter = 0;
+    let X = [], Y=[];
     for (let i=0; i < autoCompletions.length; i++){
-        autoCompletionEntries.push({
-            timestamp: autoCompletions[i].timestamp,
-            modelName: autoCompletions[i].modelName,
-            eventName: autoCompletions[i].eventName,
-            prompt: autoCompletions[i].prompt,
-            completion: autoCompletions[i].completion,
-            accepted: autoCompletions[i].accepted,
-            filepath: autoCompletions[i].filepath
-        });
+        // TODO: make this work with '*' extensions, e.g. all the files, an empty
+        // string causes all files to be filters
+        if(opts.extension && autoCompletions[i].filepath.endsWith(opts.extension)){
+            X.push(new Date(autoCompletions[i].timestamp));
+            Y.push(autoCompletions[i].completion.length);
+            autoCompletionEntries.push({
+                timestamp: new Date(autoCompletions[i].timestamp),
+                modelName: autoCompletions[i].modelName,
+                eventName: autoCompletions[i].eventName,
+                prompt: autoCompletions[i].prompt,
+                completion: autoCompletions[i].completion,
+                accepted: new Boolean(autoCompletions[i].accepted),
+                filepath: autoCompletions[i].filepath
+            });
+            if(autoCompletions[i].accepted){
+                acceptedCounter++;
+            } else {
+                rejectedCounter++;
+            }
+            // console.log(`processed ${new Date(autoCompletions[i].timestamp)} of type: ${typeof(new Date(autoCompletions[i].timestamp))}`);
+        }
     }
-   console.log(autoCompletionEntries[autoCompletionEntries.length-1]);
+   if(opts.verbose){
+       console.log(autoCompletionEntries[autoCompletionEntries.length-1]);
+       console.log(`processed ${autoCompletionEntries.length} autocompletes`);
+   } else {
+       console.log(`processed ${autoCompletionEntries.length} autocompletes, ${autoCompletions.length - autoCompletionEntries.length} were filtered out`);
+       // console.log(
+   }
+    console.log(`Of these ${acceptedCounter} were accepted, ${rejectedCounter} were rejected`);
+    if(opts.plot){
+        const data = [{x: X, y: Y, type: 'scatter'}];
+        plot(data);
+    }
 };
 
 program
   .name('mycli')
   .description('A simple CLI tool example')
   .option('-v, --verbose', 'Enables verbose mode')
-  .option('-f, --filepath', 'Absolute filepath for the autocompletes file')
+  .option('-f, --filepath <string>', 'Absolute filepath for the autocompletes file', '/Users/rlucas7/.continue/dev_data/0.2.0/autocomplete.jsonl')
+  .option('-e, --extension <string>', 'Filter the devData to only include file extensions of the given string', '.js')
+  .option('-p, --plot', 'Enables plotting the devData')
   .version('1.0.0');
 
 program.command('dev_data')
@@ -63,17 +92,10 @@ program.command('dev_data')
     const opts = program.opts();
     let autoCompleteFile = "";
     console.log(opts);
-    if (!opts.filepath){
-      autoCompleteFile = "/Users/rlucas7/.continue/dev_data/0.2.0/autocomplete.jsonl";
-      console.log(`No filepath provided. Using default path: ${autoCompleteFile}`);
-    } else {
-        autoCompleteFile = opts.filepath;
-      console.log(`Using filepath provided: ${autoCompleteFile}`);
-    }
+    autoCompleteFile = opts.filepath;
     console.log('Processing your continue devData autocompletes!');
-    let autoCompletesDevData = processJsonl(autoCompleteFile, opts);
-    console.log(`You have ${autoCompletesDevData[0]} autocompletes!`)
-    // for now lets parse all the data...
+    processJsonl(autoCompleteFile, opts);
+    console.log(`All done processing your continue devData autocompletes!`)
 });
 
 program.parse(process.argv);
