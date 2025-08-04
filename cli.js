01755bb2e7876e75496824a7d9495106ab5c278f
Lucas Roberts <rlucas7@Lucass-MacBook-Air.local>
2025-08-04 12:19:28 -0400

    add basic cli opts
      * this commit adds two cli opts
      - v/verbose: for verbose logging
      - f/filepath: for a non-default filepath of devData

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ const readline = require('readline');
 const verbose = false;
 
 
-var processJsonl = async function processJsonlFile(filePath) {
+var processJsonl = async function processJsonlFile(filePath, opts) {
     let autoCompletions = [];
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
@@ -18,7 +18,7 @@ var processJsonl = async function processJsonlFile(filePath) {
     for await (const line of rl) {
         try {
             const data = JSON.parse(line);
-            if(verbose){
+            if(opts.verbose){
                 console.log(data); // Example: log the parsed devdata-log objects
             }
             autoCompletions.push(data);
@@ -26,23 +26,40 @@ var processJsonl = async function processJsonlFile(filePath) {
             console.error('Error parsing JSON line:', error.message);
         }
     }
-    console.log('Finished processing JSONL file.');
-    console.log(`processed ${autoCompletions.length} records`);
-    console.log(`record 0 is of type ${typeof(autoCompletions[0])}`);
-    console.log(`record 30 is of type ${typeof(autoCompletions[30])}`);
+    if(opts.verbose){
+        console.log('Finished processing JSONL file.');
+        console.log(`processed ${autoCompletions.length} records`);
+        console.log(`record 0 is of type ${typeof(autoCompletions[0])}`);
+        console.log(`record 30 is of type ${typeof(autoCompletions[30])}`);
+    }
     return autoCompletions;
 };
 
 program
   .name('mycli')
   .description('A simple CLI tool example')
+  .option('-v, --verbose', 'Enables verbose mode')
+  .option('-f, --filepath', 'Absolute filepath for the autocompletes files')
   .version('1.0.0');
 
-program.command('hello')
+program.command('dev_data')
   .description('Greets the user')
   .action(() => {
+    const opts = program.opts();
+    let autoCompleteFile = "";
+    console.log(opts);
+    if (!opts.filepath){
+      autoCompleteFile = "/Users/rlucas7/.continue/dev_data/0.2.0/autocomplete.jsonl";
+      console.log(`No filepath provided. Using default path: ${autoCompleteFile}`);
+    } else {
+        autoCompleteFile = opts.filepath;
+      console.log(`Using filepath provided: ${autoCompleteFile}`);
+    }
     console.log('Processing your continue devData autocompletes!');
-    let autoCompleteFile = "/Users/rlucas7/.continue/dev_data/0.2.0/autocomplete.jsonl";
-    let autoCompletesDevData = processJsonl(autoCompleteFile);
+    let autoCompletesDevData = processJsonl(autoCompleteFile, opts);
+    // TODO: do some stuff with the data
+
 });
+
 program.parse(process.argv);
+
